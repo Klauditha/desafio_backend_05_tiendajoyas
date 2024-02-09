159de901263fd81b9a13865176ad2270b2ede029
Rimar Basaa <rimar.basaa@gmail.com>
2024-02-09 18:00:16 -0300

    se agrega ruta ruta GET/joyas

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/joyas", (req, res) => {
+    res.status(200).json({message: "ruta joyas ok"})
+})
+
 module.exports = router;
