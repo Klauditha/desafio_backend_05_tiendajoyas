81b94938f4e4e6ab9db9bbaa4290bb864d2b18d5
Rimar Basaa <rimar.basaa@gmail.com>
2024-02-10 00:54:14 -0300

    refactorizacion routes y queries. Se implementa modelo HATEOAS

--- querys/queryStrings.js
+++ querys/queryStrings.js
@@ -1,29 +1,12 @@
 const { pool } = require('../config/db');
 const format = require('pg-format');
 
-const productLimits = async ({ limits = 10}) => {
-    const query = "SELECT * FROM inventario LIMIT $1";
-    const { rows } = await pool.query(query, [limits]);
+const productQueryString = async ({ limits = 3, page = 0, order_by = "stock_ASC"}) => {
+    const [campo, direccion] = order_by.split("_");
+    const offset = (page - 1) * limits;
+    const formatQuery = format('SELECT * FROM inventario ORDER BY %s %s LIMIT %s OFFSET %s', campo, direccion, limits, offset);
+    const { rows } = await pool.query(formatQuery);
     return rows;
-};
+    };
 
-const productPage = async ({ limits = 3, page = 0}) => {
-const offset = page * limits;
-const formatQuery = format('SELECT * FROM inventario LIMIT %s OFFSET %s', limits, offset);
-const { rows } = await pool.query(formatQuery);
-return rows;
-};
-
-const productOrderBy = async ({ limits = 3, page = 0, order_by = "stock_ASC"}) => {
-const [campo, direccion] = order_by.split("_");
-const offset = page * limits;
-const formatQuery = format('SELECT * FROM inventario ORDER BY %s %s LIMIT %s OFFSET %s', campo, direccion, limits, offset);
-const { rows } = await pool.query(formatQuery);
-return rows;
-};
-
-module.exports = {
-    productLimits,
-    productPage,
-    productOrderBy
-};
+module.exports = { productQueryString };
--- routes/routesQueryString.js
+++ routes/routesQueryString.js
@@ -1,46 +1,36 @@
 const express = require("express");
 const router = express.Router();
-const { productLimits, productPage, productOrderBy } = require("../querys/queryStrings");
-/*
+const { productQueryString } = require("../querys/queryStrings");
+
+const prepararHATEOAS = (inventario) => {
+    const total = inventario.length;
+    const results = inventario.map((m) => {
+        return {
+            name: m.nombre,
+            href: `/joyas/joya/${m.id}`,
+        }
+    }).slice(0, 4);    
+
+    //data de salida
+    const HATEOAS = {
+        "totalJoyas": total,
+        results
+    };
+
+    return HATEOAS
+};
+
 router.get("/", async (req, res) => {
-    console.log(req.query);
-    res.send("soy raiz");
-});
-*/
-
-router.get("/limits", async (req, res) => {
     try {
-        const queryStrings = req.query
-        const inventario = await productLimits(queryStrings)
-        res.json(inventario);
+        const queryStrings = req.query;
+        const inventario = await productQueryString(queryStrings);
+        console.log(inventario);
+        const HATEOAS = prepararHATEOAS(inventario)
+        res.json(HATEOAS);
         
     } catch (error) {
         res.status(500).send({ error: error.message });        
-    };    
-});
-
-
-router.get("/page", async (req, res) => {
-    try {
-        const queryStrings = req.query
-        const inventario = await productPage(queryStrings)
-        res.json(inventario);
-        
-    } catch (error) {
-        res.status(500).send({ error: error.message });        
-    };    
-});
-
-
-router.get("/order_by", async (req, res) => {
-    try {
-        const queryStrings = req.query
-        const inventario = await productOrderBy(queryStrings)
-        res.json(inventario);
-        
-    } catch (error) {
-        res.status(500).send({ error: error.message });        
-    };    
+    };
 });
 
 
